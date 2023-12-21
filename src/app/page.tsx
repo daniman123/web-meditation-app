4f6090bc96e931ac1d3cf14a8b3aaa1a2b85d86a
daniman123 <daniel-saleh@hotmail.com>
2023-12-21 21:27:47 +0100

    fix: home route component child, wrong input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HomePage from "@/components/pages/homePage/HomePage";
 export default function Home() {
   return (
     <main className="h-[100dvh]">
-      <HomePage exampleProp="Hello World!" />;
+      <HomePage />;
     </main>
   );
 }
